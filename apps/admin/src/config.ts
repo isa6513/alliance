89f45bcacb155cb3a06f3d064f263919ef267ee0
Casey Manning <kcgrind@gmail.com>
2025-08-26 13:30:26 -0700

    test different server api route

--- apps/admin/src/config.ts
+++ apps/admin/src/config.ts
@@ -1,5 +1,7 @@
-import { prod_api_url } from "@alliance/shared/lib/config";
-import { getWebSocketUrl as getWebSocketUrlShared } from "@alliance/shared/lib/config";
+import {
+  base_url,
+  getWebSocketUrl as getWebSocketUrlShared,
+} from "@alliance/shared/lib/config";
 
 export const getWebSocketUrl = (): string => {
   return getWebSocketUrlShared(import.meta.env.MODE);
@@ -13,6 +15,6 @@ export const getApiUrl = (): string => {
   if (import.meta.env.MODE === "development") {
     return "http://localhost:3005";
   } else {
-    return prod_api_url;
+    return base_url + "/adminapi";
   }
 };
