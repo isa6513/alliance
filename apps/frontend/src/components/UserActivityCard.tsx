949398d963740821b097d3492b3f017432f64353
Casey Manning <kcgrind@gmail.com>
2025-09-16 22:23:11 -0700

    comment button during edit ui fix

--- apps/frontend/src/components/UserActivityCard.tsx
+++ apps/frontend/src/components/UserActivityCard.tsx
@@ -178,50 +178,50 @@ const UserActivityCard = ({
               <p className="text-zinc-500">{timeSinceCompleted}</p>
             </div>
           </div>
-          <div className="flex items-center space-x-2 self-end">
-            {!isEditing && (
+          {!isEditing && (
+            <div className="flex items-center space-x-2 self-end">
               <ActivityLikeButton
                 liked={activity.likes.some((like) => like.id === self?.id)}
                 likes={activity.likes.length}
                 handleLike={() => handleLike(activity.id)}
               />
-            )}
-            {canEdit && !isEditing && (
-              <Button
-                onClick={(e) => {
-                  e.stopPropagation();
-                  handleEdit();
-                }}
-                color={ButtonColor.White}
-                className="flex flex-row gap-x-1 items-center !px-3 !py-[6px] !h-full"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                  className="w-5 h-5 text-zinc-600"
+              {canEdit && (
+                <Button
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    handleEdit();
+                  }}
+                  color={ButtonColor.White}
+                  className="flex flex-row gap-x-1 items-center !px-3 !py-[6px] !h-full"
                 >
-                  <path d="m5.433 13.917 1.262-3.155A4 4 0 0 1 7.58 9.42l6.92-6.918a2.121 2.121 0 0 1 3 3l-6.92 6.918c-.383.383-.84.685-1.343.886l-3.154 1.262a.5.5 0 0 1-.65-.65Z" />
-                  <path d="M3.5 5.75c0-.69.56-1.25 1.25-1.25H10A.75.75 0 0 0 10 3H4.75A2.75 2.75 0 0 0 2 5.75v9.5A2.75 2.75 0 0 0 4.75 18h9.5A2.75 2.75 0 0 0 17 15.25V10a.75.75 0 0 0-1.5 0v5.25c0 .69-.56 1.25-1.25 1.25h-9.5c-.69 0-1.25-.56-1.25-1.25v-9.5Z" />
-                </svg>
-                <span className="text-sm text-zinc-800">Edit</span>
-              </Button>
-            )}
-            {completed && activity.comments.length === 0 && (
-              <Button
-                onClick={(e) => {
-                  e.stopPropagation();
-                  setShowCommentForm(true);
-                }}
-                color={ButtonColor.White}
-                className="flex flex-row gap-x-1 items-center !px-3 !py-[6px] !h-full"
-              >
-                <span className="text-sm text-zinc-800 text-nowrap">
-                  Add comment
-                </span>
-              </Button>
-            )}
-          </div>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    className="w-5 h-5 text-zinc-600"
+                  >
+                    <path d="m5.433 13.917 1.262-3.155A4 4 0 0 1 7.58 9.42l6.92-6.918a2.121 2.121 0 0 1 3 3l-6.92 6.918c-.383.383-.84.685-1.343.886l-3.154 1.262a.5.5 0 0 1-.65-.65Z" />
+                    <path d="M3.5 5.75c0-.69.56-1.25 1.25-1.25H10A.75.75 0 0 0 10 3H4.75A2.75 2.75 0 0 0 2 5.75v9.5A2.75 2.75 0 0 0 4.75 18h9.5A2.75 2.75 0 0 0 17 15.25V10a.75.75 0 0 0-1.5 0v5.25c0 .69-.56 1.25-1.25 1.25h-9.5c-.69 0-1.25-.56-1.25-1.25v-9.5Z" />
+                  </svg>
+                  <span className="text-sm text-zinc-800">Edit</span>
+                </Button>
+              )}
+              {completed && activity.comments.length === 0 && (
+                <Button
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    setShowCommentForm(true);
+                  }}
+                  color={ButtonColor.White}
+                  className="flex flex-row gap-x-1 items-center !px-3 !py-[6px] !h-full"
+                >
+                  <span className="text-sm text-zinc-800 text-nowrap">
+                    Add comment
+                  </span>
+                </Button>
+              )}
+            </div>
+          )}
         </div>
 
         {completed && (
