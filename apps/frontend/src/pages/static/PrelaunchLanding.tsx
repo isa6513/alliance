2a8ad44f017fd093429b908ffc2f5023d4f36575
sidsquid <sidney.hough@gmail.com>
2025-09-23 13:26:29 -0700

    landing page improvement:

--- apps/frontend/src/pages/static/PrelaunchLanding.tsx
+++ apps/frontend/src/pages/static/PrelaunchLanding.tsx
@@ -10,19 +10,20 @@ const PrelaunchLandingPage: React.FC = () => {
     <div className="min-h-screen flex flex-col bg-white">
       <PrelaunchNavbar transparent={false} absolute={false} />
       <div className="flex-1 container mx-auto pt-8 pb-28 md:pb-56 flex flex-col px-5">
-        <div className="flex flex-col max-w-3xl mx-auto">
-          <img src={earth} className="mx-auto mb-16 w-80 md:w-100" />
-          <h2 className="text-center font-serif !font-semibold !text-4xl md:!text-7xl">
+        <div className="flex flex-col mx-auto">
+          <img src={earth} className="mx-auto mb-12 w-50" />
+          <h2 className="text-center font-serif !font-semibold !text-4xl md:!text-8xl">
             The Alliance
           </h2>
-          <p className="text-center !mt-4 text-xl md:text-2xl">
-            The Alliance is a group of people who take collective action to
-            advance humanity’s common interests.
+          <p className="max-w-2xl font-light text-center mx-auto !mt-4 text-xl md:text-3xl">
+            A group of people taking collective action to advance humanity’s
+            common interests.
           </p>
 
-          <div className="mx-auto w-full max-w-3xl flex flex-col gap-8 md:gap-12 mt-12">
+          <div className="mx-auto w-full flex flex-col mt-12 md:mt-18 *:py-12 *:border-t *:border-zinc-300">
             <MarkdownWrapper
               id="introduction"
+              maxWidth="max-w-3xl"
               markdownContent="
 
 Humanity faces many crises which are unlikely to be resolved by business as usual. Among them are extreme poverty, environmental destruction, breakdown of democratic institutions, and unsafe technological development.
@@ -33,7 +34,7 @@ While this project is ambitious, we believe it is the right time to undertake it
 
 "
             />
-            <div className="md:border border-zinc-200 md:p-8 md:-mx-8 flex flex-col text-lg gap-2 md:gap-4">
+            <div className="flex flex-col text-lg gap-2 md:gap-4 max-w-3xl">
               <p>
                 <span className="text-zinc-500">
                   What does membership require?{" "}
