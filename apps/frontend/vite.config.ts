81e8bc82a486ba0e31d2122b1998b92373ade88a
Casey Manning <kcgrind@gmail.com>
2025-10-09 10:58:51 -0700

    rework vite config to resolve shared differently for sourcemap output

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -43,32 +43,9 @@ export default defineConfig({
   resolve: {
     preserveSymlinks: true,
     dedupe: ["react", "react-dom"],
-    alias: [
-      {
-        find: "three/webgpu",
-        replacement: path.resolve(
-          __dirname,
-          "../../node_modules/globe.gl/node_modules/three/build/three.webgpu.js"
-        ),
-      },
-      {
-        find: "three/tsl",
-        replacement: path.resolve(
-          __dirname,
-          "../../node_modules/globe.gl/node_modules/three/build/three.tsl.js"
-        ),
-      },
-      {
-        find: "three",
-        replacement: path.resolve(
-          __dirname,
-          "../../node_modules/globe.gl/node_modules/three"
-        ),
-      },
-      {
-        find: "@alliance/shared",
-        replacement: path.resolve(monorepoRoot, "shared"),
-      },
-    ],
+    alias: {
+      "@alliance/shared": sharedPkg,
+      "@alliance/shared/*": path.join(sharedPkg, "/*"),
+    },
   },
 });
