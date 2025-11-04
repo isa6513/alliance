a6bc0c64a4e8ae03fadc7f55cb83b5688a4d403f
Casey Manning <kcgrind@gmail.com>
2025-11-04 13:33:06 -0800

    http latency metrics for frontend ssr

--- apps/frontend/server.js
+++ apps/frontend/server.js
@@ -2,6 +2,7 @@ import { createRequestHandler } from "@react-router/express";
 import compression from "compression";
 import express from "express";
 import morgan from "morgan";
+import io from "@pm2/io";
 
 const viteDevServer =
   process.env.NODE_ENV === "production"
@@ -23,6 +24,17 @@ app.use(compression());
 // http://expressjs.com/en/advanced/best-practice-security.html#at-a-minimum-disable-x-powered-by-header
 app.disable("x-powered-by");
 
+const ssrHttpLatency = io.histogram({
+  name: "ssr_http_latency",
+  measurement: "duration",
+  unit: "ms",
+});
+
+io.init({
+  http: true, // enable built-in HTTP metrics (latency, req/sec, etc)
+  transactions: true, // enable transaction tracing
+});
+
 // handle asset requests
 if (viteDevServer) {
   app.use(viteDevServer.middlewares);
@@ -33,6 +45,19 @@ if (viteDevServer) {
     express.static("build/client/assets", { immutable: true, maxAge: "1y" })
   );
 }
+
+app.use((req, res, next) => {
+  const start = process.hrtime.bigint();
+
+  res.on("finish", () => {
+    if (!ssrHttpLatency) return;
+    const end = process.hrtime.bigint();
+    const ms = Number(end - start) / 1_000_000;
+    ssrHttpLatency.update(ms);
+  });
+
+  next();
+});
 
 // Everything else (like favicon.ico) is cached for an hour. You may want to be
 // more aggressive with this caching.
