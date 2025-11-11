fb8a7c1cd047dd37d404d1bc249d5768a7e1708f
Casey Manning <kcgrind@gmail.com>
2025-11-11 14:18:32 -0800

    try initial route discovery

--- apps/frontend/react-router.config.ts
+++ apps/frontend/react-router.config.ts
@@ -3,6 +3,7 @@ import type { Config } from "@react-router/dev/config";
 export default {
   appDirectory: "src",
   ssr: true,
+  routeDiscovery: { mode: "initial" },
   async prerender() {
     return ["/", "/guide"];
   },
