6c4f5ce8afd5bf92038f2eb27fa8a4d0521c0a04
Casey Manning <kcgrind@gmail.com>
2025-11-19 11:18:16 -0800

    fix migration to be createdAt

--- server/migrations/1763578621003-notifSendTime.ts
+++ server/migrations/1763578621003-notifSendTime.ts
@@ -1,14 +1,31 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class NotifSendTime1763578621003 implements MigrationInterface {
-    name = 'NotifSendTime1763578621003'
+  name = 'NotifSendTime1763578621003';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "notification" ADD "sendTime" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    // 1. Add column nullable first
+    await queryRunner.query(`
+            ALTER TABLE "notification" 
+            ADD "sendTime" TIMESTAMP WITH TIME ZONE
+        `);
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "notification" DROP COLUMN "sendTime"`);
-    }
+    await queryRunner.query(`
+            UPDATE "notification"
+            SET "sendTime" = "createdAt"
+            WHERE "sendTime" IS NULL
+        `);
 
+    await queryRunner.query(`
+            ALTER TABLE "notification"
+            ALTER COLUMN "sendTime" SET NOT NULL,
+            ALTER COLUMN "sendTime" SET DEFAULT now()
+        `);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+            ALTER TABLE "notification" DROP COLUMN "sendTime"
+        `);
+  }
 }
