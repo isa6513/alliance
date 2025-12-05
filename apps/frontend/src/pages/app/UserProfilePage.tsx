f7414375d9ae65216bce31a52cb60d25ca3b0185
sidsquid <sidney.hough@gmail.com>
2025-12-05 08:54:36 -0800

    fix user display name sizd

--- apps/frontend/src/pages/app/UserProfilePage.tsx
+++ apps/frontend/src/pages/app/UserProfilePage.tsx
@@ -337,7 +337,7 @@ const UserProfilePage: React.FC = () => {
               />
             ) : (
               <div className="flex flex-row gap-3 items-center">
-                <h1 className="font-serif !font-semibold">
+                <h1 className="font-serif !font-semibold text-3xl">
                   {profile.displayName}
                 </h1>
                 {profile.staff && (
