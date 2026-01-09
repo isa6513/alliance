c34928bf11b86f2ad9f915911327c47682227a2b
Casey Manning <kcgrind@gmail.com>
2026-01-09 12:57:51 -0800

    use portal for lightbox

--- sharedweb/ui/ImageLightbox.tsx
+++ sharedweb/ui/ImageLightbox.tsx
@@ -1,5 +1,6 @@
 import { X } from "lucide-react";
 import React, { useCallback, useEffect, useState } from "react";
+import { createPortal } from "react-dom";
 
 type ImageLightboxProps = {
   images: string[];
@@ -48,47 +49,50 @@ const ImageLightbox: React.FC<ImageLightboxProps> = ({
   return (
     <>
       {renderPreview(openLightbox)}
-      {isOpen && hasImages && (
-        <div
-          className="fixed inset-0 z-[1000] bg-black/80 flex items-center justify-center"
-          onClick={closeLightbox}
-        >
+      {isOpen &&
+        hasImages &&
+        createPortal(
           <div
-            className="relative max-w-5xl p-6"
-            onClick={(e) => e.stopPropagation()}
+            className="fixed inset-0 z-[1000] bg-black/80 flex items-center justify-center"
+            onClick={closeLightbox}
           >
-            <img
-              src={images[index]}
-              className="max-h-[80vh] w-auto mx-auto rounded shadow-2xl"
-            />
-            {images.length > 1 && (
-              <>
-                <button
-                  onClick={prevImage}
-                  className="absolute left-2 top-1/2 -translate-y-1/2 bg-white/20 hover:bg-white/30 text-white rounded-full w-10 h-10 flex items-center justify-center"
-                  aria-label="Previous image"
-                >
-                  {"<"}
-                </button>
-                <button
-                  onClick={nextImage}
-                  className="absolute right-2 top-1/2 -translate-y-1/2 bg-white/20 hover:bg-white/30 text-white rounded-full w-10 h-10 flex items-center justify-center"
-                  aria-label="Next image"
-                >
-                  {">"}
-                </button>
-              </>
-            )}
-            <button
-              onClick={closeLightbox}
-              className="absolute -right-5 -top-5 hover:bg-black text-white rounded-full w-9 h-9 flex items-center justify-center font-avenir"
-              aria-label="Close"
+            <div
+              className="relative max-w-5xl p-6"
+              onClick={(e) => e.stopPropagation()}
             >
-              <X size={18} strokeWidth={3} />
-            </button>
-          </div>
-        </div>
-      )}
+              <img
+                src={images[index]}
+                className="max-h-[80vh] w-auto mx-auto rounded shadow-2xl"
+              />
+              {images.length > 1 && (
+                <>
+                  <button
+                    onClick={prevImage}
+                    className="absolute left-2 top-1/2 -translate-y-1/2 bg-white/20 hover:bg-white/30 text-white rounded-full w-10 h-10 flex items-center justify-center"
+                    aria-label="Previous image"
+                  >
+                    {"<"}
+                  </button>
+                  <button
+                    onClick={nextImage}
+                    className="absolute right-2 top-1/2 -translate-y-1/2 bg-white/20 hover:bg-white/30 text-white rounded-full w-10 h-10 flex items-center justify-center"
+                    aria-label="Next image"
+                  >
+                    {">"}
+                  </button>
+                </>
+              )}
+              <button
+                onClick={closeLightbox}
+                className="absolute -right-5 -top-5 hover:bg-black text-white rounded-full w-9 h-9 flex items-center justify-center font-avenir"
+                aria-label="Close"
+              >
+                <X size={18} strokeWidth={3} />
+              </button>
+            </div>
+          </div>,
+          document.body
+        )}
     </>
   );
 };
