26b29f41953107b998910f5c0fc1de19b62fb3e3
Charles Lien <charleslien97@gmail.com>
2026-01-22 12:46:20 -0800

    change values of isIntroductoryGroupMember

---
+++ server/migrations/1769113976463-introductory-member.ts
@@ -0,0 +1,26 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class IntroductoryMember1769113976463 implements MigrationInterface {
+  name = 'IntroductoryMember1769113976463';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+      ALTER TABLE "user"
+      SET "isIntroductoryGroupMember" boolean NOT NULL DEFAULT true
+    `);
+
+    await queryRunner.query(`
+      UPDATE "user" u
+      SET "isIntroductoryGroupMember" = false
+      FROM (
+        SELECT DISTINCT ON ("userId") "userId", "type"
+        FROM "contract_event"
+        ORDER BY "userId", "date" DESC
+      ) latest
+      WHERE u."id" = latest."userId"
+        AND latest."type" = 'signed'
+    `);
+  }
+
+  public async down(): Promise<void> {}
+}
