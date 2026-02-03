d3b4ea4555cbcaffeac55cb4042ab9b77a4db221
Casey Manning <kcgrind@gmail.com>
2026-02-03 14:06:57 -0800

    test user type

--- server/src/notifs/test-users.ts
+++ server/src/notifs/test-users.ts
@@ -78,6 +78,7 @@ export const testUser = new User({
   leaderOf: [],
   invitedCommunities: [],
   participants: [],
+  remindAboutUncompletedGroupMembers: true,
 } satisfies Omit<
   User,
   | 'friends'
