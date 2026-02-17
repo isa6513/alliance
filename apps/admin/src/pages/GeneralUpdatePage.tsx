39fe94ba2bd50444ac6a800750dc2a8e18252bbb
Charles Lien <charleslien97@gmail.com>
2026-02-17 11:02:02 -0800

    fix: remove unnecessary dom mutation

--- apps/admin/src/pages/GeneralUpdatePage.tsx
+++ apps/admin/src/pages/GeneralUpdatePage.tsx
@@ -375,7 +375,6 @@ const GeneralUpdatePage: React.FC = () => {
                   const v = e.target.value;
                   if (v) {
                     handleAddSuite(Number(v));
-                    e.target.value = "";
                   }
                 }}
                 className="rounded-md border border-gray-300 bg-white px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
