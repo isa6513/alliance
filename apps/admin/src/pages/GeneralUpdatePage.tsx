d21347ade006cbaabe2a1b8c579f8880dc2dc460
Charles Lien <charleslien97@gmail.com>
2026-02-17 14:26:45 -0800

    switch "Task Form" tab to "Fom Builder"

--- apps/admin/src/pages/GeneralUpdatePage.tsx
+++ apps/admin/src/pages/GeneralUpdatePage.tsx
@@ -637,7 +637,7 @@ const GeneralUpdatePage: React.FC = () => {
                     : "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300"
                 }`}
               >
-                Task Form
+                Form Builder
               </button>
             </nav>
           </div>
