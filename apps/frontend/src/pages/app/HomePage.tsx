df1664a27420b691ec7f7acc5a19bc0e45a1dbb3
Casey Manning <kcgrind@gmail.com>
2026-02-18 14:33:51 -0800

    typo

--- apps/frontend/src/pages/app/HomePage.tsx
+++ apps/frontend/src/pages/app/HomePage.tsx
@@ -158,8 +158,6 @@ const HomePage = () => {
       );
     }
 
-    // this is a test issue. this is a bad comment that should be removed, and it is easy to do so!!
-
     const dismissProps: LargeActionCardProps["dismissProps"] =
       !currentTask || currentTask.onboarding
         ? undefined
