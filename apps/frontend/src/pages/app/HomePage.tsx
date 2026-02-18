9dc5e04bbbe82cab5268011e91ef9b2553808816
Casey Manning <kcgrind@gmail.com>
2026-02-18 14:23:15 -0800

    with perms

--- apps/frontend/src/pages/app/HomePage.tsx
+++ apps/frontend/src/pages/app/HomePage.tsx
@@ -158,7 +158,7 @@ const HomePage = () => {
       );
     }
 
-    // this is a test issue. this is a bad comment that should be removed, and it is easy to do so
+    // this is a test issue. this is a bad comment that should be removed, and it is easy to do so!
 
     const dismissProps: LargeActionCardProps["dismissProps"] =
       !currentTask || currentTask.onboarding
