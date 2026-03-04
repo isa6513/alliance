266a3e9b1e17ca3c2dab6280b3fa946acc80e391
sidsquid <sidney.hough@gmail.com>
2026-03-04 11:54:58 -0800

    adjust icon size info pg

--- apps/frontend/src/components/InfoResourceCard.tsx
+++ apps/frontend/src/components/InfoResourceCard.tsx
@@ -19,8 +19,8 @@ const InfoResourceCard = ({
       to={href}
       className="flex flex-row gap-x-2 justify-between items-center group hover:cursor-pointer border-l-2 border-transparent pl-3 hover:border-green"
     >
-      <div className="flex flex-row gap-x-3 items-center">
-        <Icon size={24} className="text-green" />
+      <div className="flex flex-row gap-x-5 items-center">
+        <Icon className="text-green w-6 h-6 md:w-8 md:h-8 shrink-0" />
         <div>
           <h2 className="text-xl font-medium text-zinc-800 group-hover:underline">
             {title}
