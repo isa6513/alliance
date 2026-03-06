d42fd6c7cdcb5cf7e1f7e3a31d3a366c83d76c8e
Casey Manning <kcgrind@gmail.com>
2026-03-05 20:24:53 -0800

    fix user icon badge hover

--- sharedweb/ui/HoverBadge.tsx
+++ sharedweb/ui/HoverBadge.tsx
@@ -7,10 +7,10 @@ export const HoverBadge: React.FC<HoverBadgeProps> = ({
   title,
 }: HoverBadgeProps) => {
   return (
-    <div className="relative group inline-block">
+    <div className="relative group/icon inline-block">
       <div
         className="absolute top-full mt-1 left-1/2 z-30 -translate-x-1/2 whitespace-nowrap rounded border 
-      border-zinc-200 bg-white px-2 py-1 hidden group-hover:block text-black"
+      border-zinc-200 bg-white px-2 py-1 hidden group-hover/icon:block text-black"
       >
         {title}
       </div>
