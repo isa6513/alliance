826a5a9ce1742a478a687f18285d05d163228d3e
Charles Lien <charleslien97@gmail.com>
2026-03-19 16:13:37 -0700

    import from server instead of shared

--- server/src/tasks/form.dto.ts
+++ server/src/tasks/form.dto.ts
@@ -14,8 +14,8 @@ import { Form } from './entities/form.entity';
 import { FormResponse } from './entities/formresponse.entity';
 import type { Ty } from './entities/type';
 import type { AggregateViewSchema } from './schema';
-import { DEVICE_VISIBILITY_TARGETS } from '@alliance/shared/forms/schema/device';
-import type { DeviceVisibilityTarget } from '@alliance/shared/forms/schema/device';
+import { DEVICE_VISIBILITY_TARGETS } from './form-schema/v0/device';
+import type { DeviceVisibilityTarget } from './form-schema/v0/device';
 
 export class CreateFormDto extends PickType(Form, ['title', 'schema']) {}
 
