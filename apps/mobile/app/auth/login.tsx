3cf55026d993f434bdde898e6e1d56e438eab04a
Casey Manning <kcgrind@gmail.com>
2026-03-21 16:31:20 -0700

    globe icon, image cleanup, login style fixes

--- apps/mobile/app/auth/login.tsx
+++ apps/mobile/app/auth/login.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { View, Alert, StyleSheet } from "react-native";
+import { View, Alert } from "react-native";
 import { useRouter } from "expo-router";
 import { useAuth } from "../../lib/AuthContext";
 import Button from "../../components/system/Button";
@@ -36,9 +36,9 @@ const LoginScreen = () => {
   };
 
   return (
-    <View style={styles.inner} className="flex flex-col bg-zinc-100">
+    <View className="flex flex-col bg-zinc-100 py-16 flex-1 px-2">
       <View className="flex-1 pt-20 justify-start">
-        <Text className="text-3xl font-semibold mb-6">The Alliance</Text>
+        <Text className="text-3xl font-bold mb-6 px-3">The Alliance</Text>
 
         <Card cardStyle={CardStyle.White}>
           <View className="flex flex-col gap-y-6">
@@ -81,12 +81,5 @@ const LoginScreen = () => {
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  inner: {
-    padding: 16,
-    flex: 1,
-  },
-});
 
 export default LoginScreen;
