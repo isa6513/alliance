1dae41f3d8ecd786bbb232c8034c06b1532a0243
Casey Manning <kcgrind@gmail.com>
2026-03-23 19:45:04 -0700

    automatically set dev bundle id

---
+++ apps/mobile/app.config.js
@@ -0,0 +1,99 @@
+const IS_DEV = process.env.APP_VARIANT === "development";
+
+export default {
+  expo: {
+    name: IS_DEV ? "Alliance (Dev)" : "Alliance",
+    slug: "alliance-mobile",
+    version: "1.1",
+    orientation: "portrait",
+    icon: "./assets/images/globe-icon.png",
+    scheme: "alliance",
+    userInterfaceStyle: "automatic",
+    newArchEnabled: true,
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: IS_DEV
+        ? "com.alliancefoundation.alliancemobiledev"
+        : "com.alliancefoundation.alliancemobile",
+      infoPlist: {
+        ITSAppUsesNonExemptEncryption: false,
+        NSAppTransportSecurity: {
+          NSAllowsArbitraryLoads: true,
+        },
+      },
+      appleTeamId: "629G87T7R5",
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/images/globe-icon.png",
+        backgroundColor: "#000000",
+      },
+      package: IS_DEV
+        ? "com.alliance.alliancemobile.dev"
+        : "com.alliance.alliancemobile",
+      googleServicesFile: "./google-services.json",
+      versionCode: 5,
+    },
+    web: {
+      bundler: "metro",
+      output: "static",
+      favicon: "./assets/images/globe-icon.png",
+    },
+    plugins: [
+      "expo-router",
+      [
+        "expo-splash-screen",
+        {
+          image: "./assets/images/splash-icon.png",
+          imageWidth: 100,
+          resizeMode: "contain",
+          backgroundColor: "#ffffff",
+        },
+      ],
+      [
+        "expo-font",
+        {
+          fonts: [
+            "./assets/fonts/IBMPlexSans-Regular.ttf",
+            "./assets/fonts/IBMPlexSans-Medium.ttf",
+            "./assets/fonts/IBMPlexSans-Bold.ttf",
+            "./assets/fonts/SourceSans3.ttf",
+            "./assets/fonts/SourceSans3-Italic.ttf",
+            "./assets/fonts/LibreBaskerville.ttf",
+            "./assets/fonts/LibreBaskerville-Bold.ttf",
+            "./assets/fonts/LibreBaskerville-SemiBold.ttf",
+            "./assets/fonts/BerlingskeSerif-Blk.ttf",
+          ],
+        },
+      ],
+      [
+        "expo-image-picker",
+        {
+          photosPermission:
+            "The app accesses your photos to let you upload them when completing actions.",
+          cameraPermission: false,
+        },
+      ],
+      "expo-localization",
+      "expo-secure-store",
+      "expo-web-browser",
+      [
+        "expo-notifications",
+        {
+          icon: "./assets/images/globe-icon.png",
+          color: "#000000",
+        },
+      ],
+      "expo-video",
+    ],
+    experiments: {
+      typedRoutes: true,
+    },
+    extra: {
+      router: {},
+      eas: {
+        projectId: "49c13cc4-9361-4e91-8de8-27108c7527a6",
+      },
+    },
+  },
+};
