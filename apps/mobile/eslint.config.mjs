9568e057f5b4846f71f4dec60617e9d05370fe0e
Charles Lien <charleslien97@gmail.com>
2026-04-08 17:52:56 -0700

    import eslint-config-expo/flat instead of flat.js

--- apps/mobile/eslint.config.mjs
+++ apps/mobile/eslint.config.mjs
@@ -1,6 +1,6 @@
 // https://docs.expo.dev/guides/using-eslint/
 import { defineConfig } from "eslint/config";
-import expoConfig from "eslint-config-expo/flat.js";
+import expoConfig from "eslint-config-expo/flat";
 
 export default defineConfig([
   expoConfig,
